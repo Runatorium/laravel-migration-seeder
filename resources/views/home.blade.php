8a7ae0acf43a0e20082f008b3569fa96fe036728
Runatorium <stefano.alesci1@gmail.com>
2023-05-12 16:38:26 +0200

    Set Up Controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'home page')
+
+
+@section('content')
+    <h1>this is the main content</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Bootstrap
+            </a>
+        </div>
+    </nav>
+</header>
